5cb81d092e44b275232811b991e80731dbcdc125
K0nny16 <kowal.karl99@gmail.com>
2023-11-20 15:20:07 +0100

    Actionlistener / rum för toppanel
    Har gjort grunden för actionlistener

--- src/GUI.java
+++ src/GUI.java
@@ -7,12 +7,14 @@ class GUI implements ActionListener {
 
     JFrame frame;
     JPanel panel;
+    JButton[] knappar;
 
     GUI(){
         frame = new JFrame();
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+        knappar = new JButton[9];
 
 
         //Metod för toppanel.
@@ -25,13 +27,22 @@ class GUI implements ActionListener {
         panel.setLayout(new GridLayout(3,3,1,1));
         frame.add(panel,BorderLayout.CENTER);
         for(int i = 0; i < 9; i++){
-            JButton knapp = new JButton();
-            panel.add(knapp);
+            knappar[i] = new JButton();
+            knappar[i].addActionListener(this);
+            panel.add(knappar[i]);
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JButton source = (JButton) e.getSource();
 
+        for(int i = 0; i < 9; i++){
+            if(source==knappar[i]){
+                knappar[i].setText("XXXxxxXXXXXXX");
+                panel.revalidate();
+                panel.repaint();
+            }
+        }
     }
 }

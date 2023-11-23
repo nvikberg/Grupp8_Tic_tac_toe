f91680f8f01ab4018ce02db3b9a105dfef01ed89
Rositsa Nikolova <rositsa.se@gmail.com>
2023-11-23 17:00:57 +0100

    Added comments
    Added comments to game3. Mashreka will check them out and edit them later.

--- src/Game3.java
+++ src/Game3.java
@@ -23,27 +23,9 @@ public class Game3 extends JFrame implements ActionListener {
     ImageIcon winnerOImage = new ImageIcon("yellowWinner.png");
 
 
-
-
-
     public Game3() {
-        //adding UI manager "Look and Feel design Nimbus to the game /nv
-        try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-                    break;
-                }
-            }
-        } catch (Exception ignored) {
-        }
-        //key theme inputs for nimbus with a color change /nv
-        UIManager.put("nimbusBase", new Color(149, 212, 163));
-        UIManager.put("nimbusBlueGrey", new Color(248, 240, 131));
-        UIManager.put("control", new Color(234, 119, 133));
-       // UIManager.put("text", new Color( 255,255,255));
-        UIManager.put("TextArea.background", new Color(149, 212, 163));
-        UIManager.put("TextField.background", new Color(149, 212, 163));
+        //Add the color schemes
+        setColorScheme();
 
         setTitle("Tic Tac Toe");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -71,6 +53,8 @@ public class Game3 extends JFrame implements ActionListener {
         startButton = new JButton("Start Game");
         startButton.setHorizontalAlignment(SwingConstants.CENTER);
         startButton.addActionListener(this);
+
+        // the Separator takes the next space otherwise currenPlayer ends up there.
         playerPanel.add(startButton);
         playerPanel.add(new JSeparator(SwingConstants.VERTICAL));
 
@@ -91,7 +75,7 @@ public class Game3 extends JFrame implements ActionListener {
         scoreLabel = new JLabel(playerXField.getText() + " : " + playerXScore + " " + playerOField.getText() + ": " + playerOScore);
         scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        // If players want to play a new game
+        // If players want to play a new game. Can change names again and the score nullates in ActionListeners
         restartButton = new JButton("RESET GAME");
         restartButton.addActionListener(this);
 
@@ -111,70 +95,11 @@ public class Game3 extends JFrame implements ActionListener {
         setVisible(true);
     }
 
-    //initializeButtons in game panel
-    private void initializeButtons(JPanel gamePanel) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                buttons[i][j] = new JButton();
-                buttons[i][j].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
-                buttons[i][j].addActionListener(this);
-                gamePanel.add(buttons[i][j]);
-                buttons[i][j].setEnabled(false);
-            }
-        }
-    }
-
-    // If players want to play a new game
-    //reset game (create a new blank game panel)
-    private void restartGame() {
-        turnCount = 0;
-        playerXturn = random.nextInt() < 0.5;// random boolean value where there's a 50% chance of it being true
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                buttons[i][j].setText("");
-                buttons[i][j].setEnabled(true);
-            }
-        }
-        if (playerXturn) {
-            currentPlayer.setText(playerXField.getText() + "'s turn!");
-        } else {
-            currentPlayer.setText(playerOField.getText() + "'s turn!");
-        }
-    }
-    // en method for check winner
-    private boolean checkWin(String symbol) {
-        boolean isWinner = false;
-        for (int i = 0; i < 3; i++) {
-            if (buttons[i][0].getText().equals(symbol) && buttons[i][1].getText().equals(symbol) && buttons[i][2].getText().equals(symbol)) {
-                isWinner = true;
-            }
-            if (buttons[0][i].getText().equals(symbol) && buttons[1][i].getText().equals(symbol) && buttons[2][i].getText().equals(symbol)) {
-                isWinner = true;
-            }
-        }
-        if (buttons[0][0].getText().equals(symbol) && buttons[1][1].getText().equals(symbol) && buttons[2][2].getText().equals(symbol)) {
-            isWinner = true;
-        }
-        if (buttons[0][2].getText().equals(symbol) && buttons[1][1].getText().equals(symbol) && buttons[2][0].getText().equals(symbol)) {
-            isWinner = true;
-        }
-        if (!isWinner) {
-            if (symbol.equals("X")) {
-                currentPlayer.setText(playerOField.getText() + "'s turn!");
-            } else {
-                currentPlayer.setText("" + playerXField.getText() + "'s turn!");
-            }
-        } else {
-            currentPlayer.setText("");
-        }
-        return isWinner;
-    }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton clickedButton = (JButton) e.getSource();
-        //Action when the start button is clicked
+        //Action when the start button is clicked. Sets the variables on 0 (the player score is already set somewhere else and can be removed)
         if (clickedButton == startButton) {
             startButton.setEnabled(false);
             playerXField.setEnabled(false);
@@ -184,11 +109,12 @@ public class Game3 extends JFrame implements ActionListener {
             scoreLabel.setText(playerXField.getText() + ": " + playerXScore + " " + playerOField.getText() + ": " + playerOScore);
             resultTextArea.setText("");
             restartButton.setEnabled(true);
+            // in the metod restartGame the first player is decided. PlayerX true == playerX is first PlayerX false == PlayerO is first
             restartGame();
             return;
         }
 
-        //Action when the restart button is clicked
+        //Action when the Reset Game is clicked. Removes the scores of the previous players and let them chose new names
         if (clickedButton == restartButton) {
             playerXScore = 0;
             playerOScore = 0;
@@ -207,10 +133,15 @@ public class Game3 extends JFrame implements ActionListener {
             currentPlayer.setText("");
             return;
         }
+        // If there is text in the currently pressed button the actionListener is cancelled and nothing happens.
+        // that way we avoid disabling the currently pressed button.
         if (!clickedButton.getText().isEmpty()) {
             return;
         }
-        //Action for  playerX turn
+        //Action for  playerX turn. It repeats what happens in PlayerO turn. PlayerXturn holds value True or False.
+        //If the current player is player X (Player one) then mark the button with his symbol, play the sound,
+        // check if with that move the player X wins (checkWin returns true or false) if true do actions, if false do other actions and change the player order
+        // by setting playerXturn = false;
         if (playerXturn) {
             clickedButton.setText("X");
             //clickedButton.setEnabled(false);
@@ -246,7 +177,8 @@ public class Game3 extends JFrame implements ActionListener {
                     playerXturn = false;
                 }
             }
-            //Action for  playerO turn
+            // The same thing like with the player playing with X. Repetition. At the end if player O is not a winner switch the turn back to Player X
+            // by setting playerXturn = true;
         } else {
             clickedButton.setText("O");
             //clickedButton.setEnabled(false);
@@ -255,7 +187,6 @@ public class Game3 extends JFrame implements ActionListener {
             } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
                 throw new RuntimeException(ex);
             }
-            currentPlayer.setText(playerOField.getText() + "'s turn!");
             // call checkwin method to check PlayerO is winner or not
             if (checkWin("O")) {
                 try {
@@ -285,7 +216,78 @@ public class Game3 extends JFrame implements ActionListener {
         }
     }
 
-    // to check maximumScore and add it result textarea
+
+    //initializeButtons in game panel
+    private void initializeButtons(JPanel gamePanel) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
+                buttons[i][j].addActionListener(this);
+                gamePanel.add(buttons[i][j]);
+                buttons[i][j].setEnabled(false);
+            }
+        }
+    }
+
+
+    // If players want to play a new game
+    //restart game (creates a new blank game panel). Desides if playerX will be first or second. Writes who's turn it is.
+    //Restart game is used every time since the button start game has been clicked. Called after a win for any of the players or a draw.
+    // It is also called when the button Start Game is hit.
+    private void restartGame() {
+        turnCount = 0;
+        playerXturn = random.nextInt() < 0.5;// random boolean value where there's a 50% chance of it being true
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setEnabled(true);
+            }
+        }
+        if (playerXturn) {
+            currentPlayer.setText(playerXField.getText() + "'s turn!");
+        } else {
+            currentPlayer.setText(playerOField.getText() + "'s turn!");
+        }
+    }
+
+
+    // en method for check winner
+    private boolean checkWin(String symbol) {
+        boolean isWinner = false;
+        for (int i = 0; i < 3; i++) {
+            // First if -> checks if all the buttons of the same ROW are holding the same player symbol. If yes - this is the winner of the game
+            if (buttons[i][0].getText().equals(symbol) && buttons[i][1].getText().equals(symbol) && buttons[i][2].getText().equals(symbol)) {
+                isWinner = true;
+            }
+            // Second if -> checks if all the buttons of the same COLUMN are holding the same player symbol. If yes - this is the winner of the game
+            if (buttons[0][i].getText().equals(symbol) && buttons[1][i].getText().equals(symbol) && buttons[2][i].getText().equals(symbol)) {
+                isWinner = true;
+            }
+        }
+        // Third if -> checks if all the buttons of DIAGONAL left to right are holding the same player symbol. If yes - this is the winner of the game
+        if (buttons[0][0].getText().equals(symbol) && buttons[1][1].getText().equals(symbol) && buttons[2][2].getText().equals(symbol)) {
+            isWinner = true;
+        }
+        // Forth if -> checks if all the buttons of DIAGONAL right to left are holding the same player symbol. If yes - this is the winner of the game
+        if (buttons[0][2].getText().equals(symbol) && buttons[1][1].getText().equals(symbol) && buttons[2][0].getText().equals(symbol)) {
+            isWinner = true;
+        }
+        //If there is no winner the text showing which is the current player switches from the current to the next player.
+        if (!isWinner) {
+            if (symbol.equals("X")) {
+                currentPlayer.setText(playerOField.getText() + "'s turn!");
+            } else {
+                currentPlayer.setText("" + playerXField.getText() + "'s turn!");
+            }
+        } else {
+            currentPlayer.setText("");  // if there is a winner then the current player text is set to empty before assigning the first player of the next round.
+        }
+        return isWinner;
+    }
+
+
+    // to check who has bigger overall score and add it result textarea
     public void checkmaximumScore() {
         if (playerXScore > playerOScore) {
             resultTextArea.append(playerXField.getText() + " is the current leader.\n");
@@ -296,6 +298,7 @@ public class Game3 extends JFrame implements ActionListener {
         }
     }
 
+
     //a method for add click game button sound
     public void addClickSound() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         AudioInputStream clickAudio = AudioSystem.getAudioInputStream(new File("Pen Clicking (online-audio-converter.com).wav"));
@@ -304,11 +307,34 @@ public class Game3 extends JFrame implements ActionListener {
         clip.start();
     }
 
+
     // a method for add win sound
     public void addWinSound() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         AudioInputStream winSound = AudioSystem.getAudioInputStream(new File("Winner.wav"));
         clip = AudioSystem.getClip();
         clip.open(winSound);
         clip.start();
+    }
+
+
+    // Here is all the Color schemes that were chosen to represent the project. They are called in the constructor on start.
+    public void setColorScheme(){
+        //adding UI manager "Look and Feel design Nimbus to the game /nv
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+                    break;
+                }
+            }
+        } catch (Exception ignored) {
+        }
+        //key theme inputs for nimbus with a color change /nv
+        UIManager.put("nimbusBase", new Color(149, 212, 163));
+        UIManager.put("nimbusBlueGrey", new Color(248, 240, 131));
+        UIManager.put("control", new Color(234, 119, 133));
+        //UIManager.put("text", new Color( 255,255,255));
+        UIManager.put("TextArea.background", new Color(149, 212, 163));
+        UIManager.put("TextField.background", new Color(149, 212, 163));
     }
 }
